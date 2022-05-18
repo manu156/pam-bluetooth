48c5a15c5009b2d1839c1dab0d69af7c6175ec1b
manu <manu156@users.noreply.github.com>
2022-05-18 18:40:51 +0530

    improvement
    turn on Bluetooth automatically.
    disconnect after connecting for verification

--- pam_bluetooth.c
+++ pam_bluetooth.c
@@ -13,16 +13,27 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 }
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-	char* mac = ""; //enter mac address of device
+	char* mac = "14:07:1D:04:28:3D"; //enter mac address of device
 	char cmd[100] = "";
-	snprintf(cmd, sizeof(cmd), "timeout 5 bluetoothctl -- connect %s > /dev/null 2>&1", mac);
+	
+	/*snprintf(cmd, sizeof(cmd), "bluetoothctl -- info %s | grep -q 'Connected: yes'", mac);
+	int x = system(cmd);
+	if (x != 0){
+		snprintf(cmd, sizeof(cmd), "bluetoothctl disconnect %s", mac);
+		system(cmd);
+		return PAM_SUCCESS;
+	}*/
+	
+	snprintf(cmd, sizeof(cmd), "rfkill unblock bluetooth && timeout 5 bluetoothctl -- connect %s > /dev/null 2>&1", mac);
 	system(cmd);
 	snprintf(cmd, sizeof(cmd), "bluetoothctl -- info %s | grep -q 'Connected: yes'", mac);
 	int x = system(cmd);
 	if (x != 0){
 		return PAM_IGNORE;
 	}
+	snprintf(cmd, sizeof(cmd), "bluetoothctl disconnect %s > /dev/null 2>&1", mac);
+	system(cmd);
 	return PAM_SUCCESS;
+	//return PAM_IGNORE;
 }
-
 
